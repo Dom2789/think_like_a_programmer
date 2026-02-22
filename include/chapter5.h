a8f4e16359ca912ef8f60adadbbc425e58b2d833
Dominik Hoch <dominik.hoch.1989@freenet.de>
2026-02-22 21:43:48 +0100

    create basis for class in chapter5

--- include/chapter5.h
+++ include/chapter5.h
@@ -5,6 +5,21 @@
 namespace chapter5
 {
     void hello_world();
+    class student_record {
+        public:
+            student_record();
+            student_record(int new_grade, int new_id, std::string new_name);
+            int get_grade();
+            void set_grade(int new_grade);
+            int get_id() const;
+            void set_id(int new_id);
+            std::string get_name();
+            void set_name(std::string new_name);
+        private:
+            int _grade;
+            int _id;
+            std::string _name;
+    };
 }
 
 
--- src/chapter5.cpp
+++ src/chapter5.cpp
@@ -6,4 +6,43 @@ namespace chapter5
     {
         std::cout << "Hello World 5\n";
     }
-}
+
+    int student_record::get_grade() {
+        return _grade;
+    };
+
+    void student_record::set_grade(int new_grade) {
+        if (new_grade >= 0 && new_grade <= 100) {
+            _grade = new_grade;
+        };
+    };
+
+    int student_record::get_id() const {
+        return _id;
+    };
+    void student_record::set_id(int new_id) {
+        _id = new_id;
+    };
+
+    std::string student_record::get_name() {
+        return _name;
+    };
+
+    void student_record::set_name(std::string new_name) {
+        _name = new_name;
+    };
+
+    // constructor
+    student_record::student_record(int new_grade, int new_id, const std::string new_name) {
+        set_grade(new_grade);
+        set_id(new_id);
+        set_name(new_name);
+    };
+
+    // default constructor
+    student_record::student_record() {
+        set_grade(0);
+        set_id(-1);
+        set_name("");
+    };
+};
